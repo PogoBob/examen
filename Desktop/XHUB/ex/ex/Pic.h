92ce98a27a6bd1c56e3ef830481d47d6ad27c84e
Egor <113592940+PogoBob@users.noreply.github.com>
2023-01-17 12:18:41 +0300

    Examine

---
+++ Desktop/XHUB/ex/ex/Pic.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+class Picture_Dobrozorov
+{
+public:
+    void read();
+    void write();
+};
---
+++ Desktop/XHUB/ex/ex/main.cpp
@@ -0,0 +1,9 @@
+#include "Pic.h"
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Picture_Dobrozorov pic;
+    pic.read();
+    pic.write();
+}
